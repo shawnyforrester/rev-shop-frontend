4352370abf01c20beb2a32b11151014c1ea41ef5
S-Yohan <114297247+S-Yohan@users.noreply.github.com>
2023-04-14 10:18:16 -0400

    added registration, admin and retailer links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import{ HomeComponent } from './pages/home/home.component';
 import { ItemPageComponent } from './pages/item-page/item-page.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { ProductsPageComponent } from './pages/products-page/products-page.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'item/:id', component: ItemPageComponent }
+  { path: 'products/item/:id', component: ItemPageComponent },
+  { path: 'registration', component: RegistrationComponent },
+  { path: 'products', component: ProductsPageComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ItemPageComponent } from './pages/item-page/item-page.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProductsPageComponent } from './pages/products-page/products-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ItemPageComponent,
-    NavBarComponent
+    NavBarComponent,
+    RegistrationComponent,
+    LoginComponent,
+    ProductsPageComponent
   ],
   imports: [
     BrowserModule,
